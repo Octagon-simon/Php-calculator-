84197cd727b6b22945e43cfe355055fca6edab8a
Simon <68190998+Octagon-simon@users.noreply.github.com>
2020-07-12 21:38:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -75,7 +75,7 @@ if ($_POST)
     BASIC CALCULATOR
         </h3>
     <hr />
-    <link type="text/css" href="css/bootstrap.min.css" rel="stylesheet" />
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <div align="center">
     <form method="post">
     <label>Enter two numbers</label><br>
